377ff349c3a95f0fd5211df1524f04f82266c887
RaghuChowdary <raghunandan.balusu@valpo.edu>
2024-08-15 17:57:19 -0500

    initial commit

--- init.sh
+++ init.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # Replace variables in init.template.sql
-envsubst < /docker-entrypoint-initdb.d/init.template.sql > /docker-entrypoint-initdb.d/init.sql
+sed "s/\${MYSQL_DATABASE}/${MYSQL_DATABASE}/g; s/\${MYSQL_USER_DATABASE}/${MYSQL_USER_DATABASE}/g; s/\${MYSQL_USER}/${MYSQL_USER}/g" /docker-entrypoint-initdb.d/init.template.sql > /docker-entrypoint-initdb.d/init.sql
 
-# Execute the SQL file
+# Wait for MariaDB to be ready
+until mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "SELECT 1;" >/dev/null 2>&1; do
+  echo "Waiting for database connection..."
+  sleep 2
+done
+
+# Execute the SQL file using the default root user
 mysql -u root -p"$MYSQL_ROOT_PASSWORD" < /docker-entrypoint-initdb.d/init.sql
