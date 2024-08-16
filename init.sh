7339bb72bc276a337d7d1440b6b6d7b86aed300d
RaghuChowdary <raghunandan.balusu@valpo.edu>
2024-08-15 20:39:22 -0500

    initial commit

--- init.sh
+++ init.sh
@@ -5,11 +5,6 @@ chown -R $(id -u):$(id -g) /docker-entrypoint-initdb.d
 
 source ${HOME}/secrets/.env
 
-# Print the environment variables for debugging
-echo "MYSQL_DATABASE: ${MYSQL_DATABASE}"
-echo "MYSQL_USER_DATABASE: ${MYSQL_USER_DATABASE}"
-echo "MYSQL_USER: ${MYSQL_USER}"
-
 # Substitute environment variables in the SQL file
 sed "s|\${MYSQL_DATABASE}|${MYSQL_DATABASE}|g; s|\${MYSQL_USER_DATABASE}|${MYSQL_USER_DATABASE}|g; s|\${MYSQL_USER}|${MYSQL_USER}|g" \
 /docker-entrypoint-initdb.d/init-template.sql > /tmp/init-substituted.sql
