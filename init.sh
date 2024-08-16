fe7f211454e5ecd4cbf27da5ec5d460a851d3721
RaghuChowdary <raghunandan.balusu@valpo.edu>
2024-08-15 20:38:27 -0500

    initial commit

--- init.sh
+++ init.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # chmod +x init.sh
-source ${HOME}/secrets/.env
-
 # Change ownership of the init directory to the current running user
 chown -R $(id -u):$(id -g) /docker-entrypoint-initdb.d
+
+source ${HOME}/secrets/.env
+
+# Print the environment variables for debugging
+echo "MYSQL_DATABASE: ${MYSQL_DATABASE}"
+echo "MYSQL_USER_DATABASE: ${MYSQL_USER_DATABASE}"
+echo "MYSQL_USER: ${MYSQL_USER}"
 
 # Substitute environment variables in the SQL file
 sed "s|\${MYSQL_DATABASE}|${MYSQL_DATABASE}|g; s|\${MYSQL_USER_DATABASE}|${MYSQL_USER_DATABASE}|g; s|\${MYSQL_USER}|${MYSQL_USER}|g" \
