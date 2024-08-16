93ce6c6a44a6d4081f561c58f50fc973efd1e1af
RaghuChowdary <raghunandan.balusu@valpo.edu>
2024-08-15 20:34:28 -0500

    initial commit

--- init.sh
+++ init.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # chmod +x init.sh
+source ${HOME}/secrets/.env
 
 # Change ownership of the init directory to the current running user
 chown -R $(id -u):$(id -g) /docker-entrypoint-initdb.d
 
 # Substitute environment variables in the SQL file
 sed "s|\${MYSQL_DATABASE}|${MYSQL_DATABASE}|g; s|\${MYSQL_USER_DATABASE}|${MYSQL_USER_DATABASE}|g; s|\${MYSQL_USER}|${MYSQL_USER}|g" \
-/docker-entrypoint-initdb.d/init.sql > /tmp/init-substituted.sql
+/docker-entrypoint-initdb.d/init-template.sql > /tmp/init-substituted.sql
 
 # Move the substituted file back to the init directory
-mv /tmp/init-substituted.sql /docker-entrypoint-initdb.d/init-substituted.sql
+mv /tmp/init-substituted.sql /docker-entrypoint-initdb.d/init.sql
 
 # Start MariaDB
 exec /usr/local/bin/docker-entrypoint.sh mysqld
