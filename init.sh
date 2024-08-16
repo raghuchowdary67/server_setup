13cf0ec5d3ec69648beb3995c9dd6e5daeb4096e
RaghuChowdary <raghunandan.balusu@valpo.edu>
2024-08-15 20:58:36 -0500

    initial commit

--- init.sh
+++ init.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 # chmod +x init.sh
-# Change ownership of the init directory to the current running user
+
+# Print environment variables for debugging
+echo "MYSQL_DATABASE: ${MYSQL_DATABASE}"
+echo "MYSQL_USER_DATABASE: ${MYSQL_USER_DATABASE}"
+echo "MYSQL_USER: ${MYSQL_USER}"
+
+# Change ownership of the init directory
 chown -R $(id -u):$(id -g) /docker-entrypoint-initdb.d
 
 # Substitute environment variables in the SQL file
 sed "s|\${MYSQL_DATABASE}|${MYSQL_DATABASE}|g; s|\${MYSQL_USER_DATABASE}|${MYSQL_USER_DATABASE}|g; s|\${MYSQL_USER}|${MYSQL_USER}|g" \
 /docker-entrypoint-initdb.d/init-template.sql > /tmp/init-substituted.sql
 
-# Move the substituted file back to the init directory
-mv /tmp/init-substituted.sql /docker-entrypoint-initdb.d/init.sql
+# Move the substituted file to the init directory
+mv /tmp/init-substituted.sql /docker-entrypoint-initdb.d/init-substituted.sql
+
+# Confirm the file was created
+ls -l /docker-entrypoint-initdb.d/
 
 # Start MariaDB
-#exec /usr/local/bin/docker-entrypoint.sh mysqld
-# Execute the SQL file using the default root user
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" < /docker-entrypoint-initdb.d/init.sql
+exec /usr/local/bin/docker-entrypoint.sh mysqld
