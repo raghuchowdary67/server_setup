e38713fc0db61cc3d51ed22dcce5e66ea6349f4d
RaghuChowdary <raghunandan.balusu@valpo.edu>
2025-01-15 18:12:17 -0600

    initial commit

--- init.sh
+++ init.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # chmod +x init.sh
 
-# Change ownership of the init directory
-#chown -R $(id -u):$(id -g) /docker-entrypoint-initdb.d
-
 # Debugging: Print current user and group
 echo "Current user: $(id -u)"
 echo "Current group: $(id -g)"
@@ -13,6 +10,11 @@ echo "Current user info: $(id)"
 ls -ld /docker-entrypoint-initdb.d
 ls -l /docker-entrypoint-initdb.d
 
+# Change ownership of the init directory if not owned by the current user
+if [ "$(stat -c '%u' /docker-entrypoint-initdb.d)" != "$(id -u)" ]; then
+    echo "Changing ownership of /docker-entrypoint-initdb.d to $(id -u):$(id -g)"
+    chown -R $(id -u):$(id -g) /docker-entrypoint-initdb.d
+fi
 
 # Substitute environment variables in the SQL file
 sed "s|\${MYSQL_DATABASE}|${MYSQL_DATABASE}|g; s|\${MYSQL_USER_DATABASE}|${MYSQL_USER_DATABASE}|g; s|\${MYSQL_USER}|${MYSQL_USER}|g" \
