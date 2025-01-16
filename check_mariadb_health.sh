49b37e267b8fdba37625217db478a34c5a2cb85a
RaghuChowdary <raghunandan.balusu@valpo.edu>
2025-01-15 22:39:50 -0600

    initial commit

--- check_mariadb_health.sh
+++ check_mariadb_health.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #chmod +x check_mariadb_health.sh
 source ${HOME}/secrets/.env
-mysqladmin ping -h localhost -u root -p$MYSQL_ROOT_PASSWORD
+mysqladmin ping -h localhost -u root -p$MARIADB_ROOT_PASSWORD
